d95769913689b13ea44ea102c382910fa4224fe5
sajdeen <sajdeen0786@gmail.com>
2024-08-02 18:37:39 +0530

    Done

---
+++ Day3.js
@@ -0,0 +1,14 @@
+//Day3: Countrol Structures
+// Act1 :If-Else Statements
+//Task1: wap to check if a number is positive, negative, or zero, and log the result to the console.
+
+function checkNumber(num){
+    if (num > 0) {
+        console.log(num + "is positive.");
+    }else if(num < 0){
+        console.log(num + "is negative.");
+
+    }else{
+        console.log(num + "is zero.");
+    }
+}
